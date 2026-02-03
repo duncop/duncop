3fb3063c25f0b95f737764bcaef40bf49fb4d03d
Hayoung Kang <rkdgkdudsam@naver.com>
2026-02-03 13:55:41 +0900

    feat: Footer 컴포넌트 퍼블리싱

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer className="w-full h-15 pr-20 border-t flex gap-5 items-center justify-end border-gray300 dark:border-gray900">
+            <Link
+                href="/"
+                className="p-3 text-gray700 hover:text-gray900 dark:text-gray500 dark:hover:text-gray300"
+            >
+                도움말
+            </Link>
+            <Link
+                href="/"
+                className="p-3  text-gray700 hover:text-gray900 dark:text-gray500 dark:hover:text-gray300"
+            >
+                문의하기
+            </Link>
+            <Link
+                href="/"
+                className="p-3  text-gray700 hover:text-gray900 dark:text-gray500 dark:hover:text-gray300"
+            >
+                개인정보처리방침
+            </Link>
+            <Link
+                href="https://developers.neople.co.kr/"
+                className="p-3 group text-gray700 hover:text-gray900 dark:text-gray500 dark:hover:text-gray300"
+            >
+                <span className="text-gray400 group-hover:text-gray500 dark:text-gray700 dark:group-hover:text-gray600">
+                    powered by
+                </span>{" "}
+                <span className="text-neople">Neople</span> OpenAPI
+            </Link>
+        </footer>
+    );
+}
