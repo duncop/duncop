d96ad5fff65eecb390e3a5458aba51ffc4d1903d
Hayoung Kang <rkdgkdudsam@naver.com>
2026-02-03 13:36:19 +0900

    feat: Badge 컴포넌트 퍼블리싱

---
+++ src/components/Badge.tsx
@@ -0,0 +1,20 @@
+import { CircleStar, TriangleAlert } from "lucide-react";
+
+export default function Badge({ type }: { type: string }) {
+    return (
+        <div
+            className={`px-2 py-1 rounded-sm flex gap-1 items-center ${
+                type === "시민상"
+                    ? "bg-badge-green text-text-green dark:bg-badge-green-dark dark:text-main"
+                    : "bg-badge-red text-text-red dark:bg-badge-red-dark dark:text-red"
+            }`}
+        >
+            {type === "시민상" ? (
+                <CircleStar size={20} />
+            ) : (
+                <TriangleAlert size={20} />
+            )}
+            {type}
+        </div>
+    );
+}
