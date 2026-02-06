de17feacc868b750a561e13dcfaaded803c6e774
Hayoung Kang <rkdgkdudsam@naver.com>
2026-02-06 16:59:02 +0900

    fix: Main 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AdventureCard from "@/components/AdventureCard";
 import Menual from "@/components/Menual";
 import SearchForm from "@/components/SearchForm";
 
@@ -5,10 +6,21 @@ export default function Home() {
     return (
         <div className="gap-10 flex flex-col items-center">
             <h1>
-                DUN<span className="text-main">COP</span>
+                DUN<span className="t-main">COP</span>
             </h1>
             <SearchForm />
-            <Menual />
+            {true ? (
+                <div className="w-full">
+                    <p className="text-lg mb-2">
+                        검색 결과 <span className="t-main">1</span>
+                    </p>
+                    <div className="w-full flex gap-3">
+                        <AdventureCard />
+                    </div>
+                </div>
+            ) : (
+                <Menual />
+            )}
         </div>
     );
 }
