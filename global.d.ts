f3752dc517498c68b2548729cf6c06ca4b7ffc20
Hayoung Kang <rkdgkdudsam@naver.com>
2026-02-07 18:06:12 +0900

    chore: global type 설정

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+    interface FormType {
+        dealerCut: string;
+        bufferCut: string;
+        setCount: string;
+        babyCount: string;
+        adventure: string;
+    }
+}
